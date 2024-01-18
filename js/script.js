235562a9a4ad05e0c92a31562e1f92b29ab90532
Leninhapires <francilenesouza@hotmail.com>
2024-01-17 21:31:51 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'img/manhA.png'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'img/tarde.png'
+    } else {
+        //BOA NOITE
+        img.src = 'img/noitE.png'
+    }
+}
+
+
+
+console.log('olaa')
